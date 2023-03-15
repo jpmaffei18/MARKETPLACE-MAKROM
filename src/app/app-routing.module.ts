8470919d05c8529bbff462e1a8ae59cc284783dd
jpmaffei <jpmaffei18@gmail.com>
2023-03-15 17:44:42 -0300

    atualização_15_03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'pagamentos',
+    loadChildren: () => import('./pagamentos/pagamentos/pagamentos.module').then( m => m.PagamentosPageModule)
+  },
 ];
 
 @NgModule({
